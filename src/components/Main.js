c61f3aa09216e0fdc587c3276127517838460987
le0zh0u <492753662@qq.com>
2016-08-25 09:54:41 +0800

    add gallery images && add image data json file

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,22 @@
 require('normalize.css');
 require('../styles/App.scss');
 
-import React from 'react';
+import React from "react";
+
+//获取图片相关数据
+var imageDatas = require('../data/imageData.json');
+//利用自治性函数,将图片名信息转成图片URL路径信息
+imageDatas = (function genImageURL(imageDataArr) {
+  for (var i = 0, j = imageDataArr.length; i < j; i++) {
+    var singleImageData = imageDataArr[i];
+
+    singleImageData.imagURL = require('../images/' + singleImageData.fileName);
+
+    imageDataArr[i] = singleImageData;
+  }
+
+  return imageDataArr;
+})(imageDatas);
 
 let yeomanImage = require('../images/yeoman.png');
 
@@ -9,14 +24,13 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
+        <img src={yeomanImage} alt="Yeoman Generator"/>
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
